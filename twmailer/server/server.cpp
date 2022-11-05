c2c7a00401eba4651215959d9063762861164cdd
Josip Domazet <josip.domazet.corp@gmail.com>
2022-11-05 20:51:03 +0100

    separate handle methods

--- twmailer/server/server.cpp
+++ twmailer/server/server.cpp
@@ -82,7 +82,7 @@ void Server::start_listening() {
             break;
         }
 
-        // START CLIENT
+        // Start client
         printf("Client connected from %s:%d...\n", inet_ntoa(cliaddress.sin_addr), ntohs(cliaddress.sin_port));
 
         socketA = new_socket;
@@ -106,7 +106,7 @@ void Server::start_listening() {
 // communicate with client
 void Server::handle_client_communication(int *current_socket) {
     char buffer[BUF];
-    int size;
+    long size;
 
     do {
         printf("-------------------- \n");
@@ -125,7 +125,7 @@ void Server::handle_client_communication(int *current_socket) {
             break;
         }
         printf("\nMessage received: %s\n", buffer);
-        mailer(buffer, current_socket);
+        handle_command(buffer, current_socket);
     } while (strcmp(buffer, "quit") != 0 && !abortRequested);
 
     // closes/frees the descriptor if not already
@@ -164,226 +164,245 @@ void Server::abort() {
     }
 }
 
-// handles the mailer functions (SEND, ...)
-void Server::mailer(char buffer[BUF], int *current_socket) {
+// handles the handle_command functions (SEND, ...)
+void Server::handle_command(char buffer[BUF], int *current_socket) {
 
-    int size;
+    long size;
     char *directory = spoolDir;
     char filename[BUF];
     FILE *fptr;
     bool error = false; // check if error
 
     if (strncmp(buffer, "SEND", 4) == 0) {
-        char receiver[BUF]; // username of receiver
-        bzero(buffer, BUF);
-
-        for (int i = 0; i < 4; i++) // receive all parts of send (sender, receiver, subject, message)
-        {
-            size = recv(*current_socket, buffer, BUF, 0);
-            if (size <= 0) {
-                perror("recv error");
-                break;
-            }
-            printf("Information received: %s", buffer);
-
-            if (i == 0) {
-                buffer[strlen(buffer) - 1] = '\0';
-                strcpy(filename, directory); // get path of directory for user
-                strcat(filename, "/");
-                strcat(filename, buffer);
-                // printf("filename: %s\n", filename);
-                if (mkdir(filename, 0777) == -1) // make directory
-                {
-                    if (errno == EEXIST) // ignore error if directory already exists
-                    {
-                    } else {
-                        error = true; // all other errors set bool on true
-                    }
-                }
-            } else if (i == 1 && error == false) {
-                strcpy(receiver, buffer); // save username of receiver
-                // printf("receiver: %s\n", receiver);
-            } else if (i == 2 && error == false) {
-                buffer[strlen(buffer) - 1] = '\0';
-                strcat(filename, "/");
-                strcat(filename, buffer); // get path for file (named after subject)
-                // printf("filename: %s\n", filename);
-                fptr = fopen(filename, "w"); // create file
-                if (fptr == NULL) {
-                    printf("Unable to create file.\n");
-                    error = true;
-                } else {
-                    fputs(receiver, fptr); // print receiver into file
-                }
-            } else if (i == 3 && error == false) {
-                fputs(buffer, fptr); // print message into file
-                fclose(fptr);
-            }
-        }
-
-        bzero(filename, BUF);
-        // printf("filename1: %s\n", filename);
-        bzero(receiver, BUF);
-        bzero(buffer, BUF);
-        if (!error) {
-            printf("\n\nEverything went well!\n");
-            buffer = (char *) "OK\n";
-        } else {
-            printf("\n\nThere was an error!\n");
-            buffer = (char *) "ERR\n";
-        }
-        if (send(*current_socket, buffer, BUF, 0) == -1) // send message to client
-        {
-            perror("send error");
-        }
+        handleSend(buffer, current_socket, size, directory, filename, fptr, error);
         return;
     } else if (strncmp(buffer, "LIST", 4) == 0) // list all subjects of a user
     {
-        char subjects[BUF];
-        bzero(subjects, BUF);
-        char c[BUF];
-        bzero(c, BUF);
-        int count = 0;
-        bzero(buffer, BUF);
-        size = recv(*current_socket, buffer, BUF, 0); // receive username
-        if (size <= 0) {
-            perror("list error");
-        }
-        printf("Username received: %s\n", buffer);
-
-        buffer[strlen(buffer) - 1] = '\0';
-        strcpy(filename, directory);
-        strcat(filename, "/");
-        strcat(filename, buffer); // get path of directory of user
-
-        DIR *dir;
-        struct dirent *ent;
-        if ((dir = opendir(filename)) != NULL) {
-            while ((ent = readdir(dir)) != NULL) {
-                if (ent->d_name[0] != '.' &&
-                    ent->d_name[strlen(ent->d_name) - 1] != '~') // get all files except  ., .., hidden files
-                {
-                    count++;                       // count files
-                    strcat(subjects, ent->d_name); // save them all into subjects
-                    strcat(subjects, "\n");
-                }
-            }
-            sprintf(c, "%d", count); // convert count int into char array
-            strcat(c, "\n");
-            strcat(c, subjects); // concenate count and subjects
-            bzero(subjects, BUF);
-            closedir(dir);
-        } else {
-            perror("Error when opening directory");
-            error = true;
-        }
-        printf("Subjects:\n%s", c);
-
-        if (count == 0 || error == true) {
-            bzero(c, BUF);
-            strcat(c, "0\n"); // when no user or files -> 0
-        }
-
-        if (send(*current_socket, c, BUF, 0) == -1) // send message
-        {
-            perror("list error");
-        }
-        bzero(filename, BUF);
-        bzero(c, BUF);
+        handleList(buffer, current_socket, size, directory, filename, error);
         return;
     } else if (strncmp(buffer, "READ", 4) == 0) // read specific message
     {
-        char message[BUF];
-        bzero(buffer, BUF);
-        bzero(filename, BUF);
-        for (int i = 0; i < 2; i++) {
-            recv(*current_socket, buffer, BUF, 0);
-            printf("Information received: %s", buffer);
-            if (i == 0) {
-                buffer[strlen(buffer) - 1] = '\0';
-                strcpy(filename, directory);
-                strcat(filename, "/");
-                strcat(filename, buffer); // get path of user directory
-            } else if (i == 1) {
-                buffer[strlen(buffer) - 1] = '\0';
-                strcat(filename, "/");
-                strcat(filename, buffer); // get path of specifc file (subject)
-                // printf("%s\n", filename);
-                fptr = fopen(filename, "r"); // open file to read
-                if (fptr == NULL) {
-                    perror("Unable to open file");
-                    error = true;
-                } else {
-                    bzero(buffer, BUF);
-                    printf("\n");
-                    int j = 0;
-                    while (fscanf(fptr, "%s", buffer) != EOF) // scan all lines
-                    {
-                        printf("%s\n", buffer);
-                        if (j != 0) // save all lines except first one (=receiver)
-                        {
-                            strcat(message, buffer);
-                            strcat(message, "\n");
-                        }
-                        j = 1;
-                    }
-                }
-            }
-        }
-        if (!error) {
-            printf("\n\nEverything went well!\n");
-            strcat(message, "\nOK\n");
-        } else {
-            printf("\n\nThere was an error!\n");
-            bzero(message, BUF);
-            strcat(message, "ERR\n");
-        }
-        if ((send(*current_socket, message, BUF, 0)) == -1) {
-            perror("read error");
-        }
-        bzero(buffer, BUF);
-        bzero(message, BUF);
-        bzero(filename, BUF);
-        fclose(fptr);
+        handleRead(buffer, current_socket, directory, filename, fptr, error);
         return;
     } else if (strncmp(buffer, "DEL", 3) == 0) // delete specific file (subject)
     {
-        bzero(buffer, BUF);
-        for (int i = 0; i < 2; i++) {
-            recv(*current_socket, buffer, BUF, 0);
-            printf("Information received: %s", buffer);
-            if (i == 0) {
-                buffer[strlen(buffer) - 1] = '\0';
-                strcpy(filename, directory);
-                strcat(filename, "/");
-                strcat(filename, buffer); // get path of username directory
-            } else if (i == 1) {
-                buffer[strlen(buffer) - 1] = '\0';
-                strcat(filename, "/");
-                strcat(filename, buffer); // get path of file (subject)
-                if (remove(filename))     // delete file
+        handleDel(buffer, current_socket, directory, filename, error);
+        return;
+    }
+    }
+
+void Server::handleDel(char buffer[1024], const int *current_socket, const char *directory, char *filename,
+                       bool error) {
+    bzero(buffer, BUF);
+    for (int i = 0; i < 2; i++) {
+        recv(*current_socket, buffer, BUF, 0);
+        printf("Information received: %s", buffer);
+        if (i == 0) {
+            buffer[strlen(buffer) - 1] = '\0';
+            strcpy(filename, directory);
+            strcat(filename, "/");
+            strcat(filename, buffer); // get path of username directory
+        } else if (i == 1) {
+            buffer[strlen(buffer) - 1] = '\0';
+            strcat(filename, "/");
+            strcat(filename, buffer); // get path of file (subject)
+            if (remove(filename))     // delete file
+            {
+                perror("Error when deleting file");
+                error = true;
+            } else {
+                printf("\nFile was deleted successfully!\n");
+            }
+        }
+    }
+    bzero(buffer, BUF);
+    if (!error) {
+        printf("\n\nEverything went well!\n");
+        strcat(buffer, "\nOK\n");
+    } else {
+        printf("\n\nThere was an error!\n");
+        strcat(buffer, "ERR\n");
+    }
+    if ((send(*current_socket, buffer, BUF, 0)) == -1) // send message
+    {
+        perror("del error");
+    }
+    bzero(buffer, BUF);
+}
+
+void Server::handleRead(char buffer[1024], const int *current_socket, const char *directory, char *filename, FILE *fptr,
+                        bool error) {
+    char message[BUF];
+    bzero(buffer, BUF);
+    bzero(filename, BUF);
+    for (int i = 0; i < 2; i++) {
+        recv(*current_socket, buffer, BUF, 0);
+        printf("Information received: %s", buffer);
+        if (i == 0) {
+            buffer[strlen(buffer) - 1] = '\0';
+            strcpy(filename, directory);
+            strcat(filename, "/");
+            strcat(filename, buffer); // get path of user directory
+        } else if (i == 1) {
+            buffer[strlen(buffer) - 1] = '\0';
+            strcat(filename, "/");
+            strcat(filename, buffer); // get path of specifc file (subject)
+            // printf("%s\n", filename);
+            fptr = fopen(filename, "r"); // open file to read
+            if (fptr == NULL) {
+                perror("Unable to open file");
+                error = true;
+            } else {
+                bzero(buffer, BUF);
+                printf("\n");
+                int j = 0;
+                while (fscanf(fptr, "%s", buffer) != EOF) // scan all lines
                 {
-                    perror("Error when deleting file");
-                    error = true;
-                } else {
-                    printf("\nFile was deleted successfully!\n");
+                    printf("%s\n", buffer);
+                    if (j != 0) // save all lines except first one (=receiver)
+                    {
+                        strcat(message, buffer);
+                        strcat(message, "\n");
+                    }
+                    j = 1;
                 }
             }
         }
-        bzero(buffer, BUF);
-        if (!error) {
-            printf("\n\nEverything went well!\n");
-            strcat(buffer, "\nOK\n");
-        } else {
-            printf("\n\nThere was an error!\n");
-            strcat(buffer, "ERR\n");
-        }
-        if ((send(*current_socket, buffer, BUF, 0)) == -1) // send message
-        {
-            perror("del error");
-        }
-        bzero(buffer, BUF);
-        return;
     }
-    return;
+    if (!error) {
+        printf("\n\nEverything went well!\n");
+        strcat(message, "\nOK\n");
+    } else {
+        printf("\n\nThere was an error!\n");
+        bzero(message, BUF);
+        strcat(message, "ERR\n");
+    }
+    if ((send(*current_socket, message, BUF, 0)) == -1) {
+        perror("read error");
+    }
+    bzero(buffer, BUF);
+    bzero(message, BUF);
+    bzero(filename, BUF);
+    fclose(fptr);
+}
+
+void Server::handleList(char buffer[1024], const int *current_socket, long size, const char *directory, char *filename,
+                        bool error) {
+    char subjects[BUF];
+    bzero(subjects, BUF);
+    char c[BUF];
+    bzero(c, BUF);
+    int count = 0;
+    bzero(buffer, BUF);
+    size = recv(*current_socket, buffer, BUF, 0); // receive username
+    if (size <= 0) {
+        perror("list error");
+    }
+    printf("Username received: %s\n", buffer);
+
+    buffer[strlen(buffer) - 1] = '\0';
+    strcpy(filename, directory);
+    strcat(filename, "/");
+    strcat(filename, buffer); // get path of directory of user
+
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(filename)) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_name[0] != '.' &&
+                ent->d_name[strlen(ent->d_name) - 1] != '~') // get all files except  ., .., hidden files
+            {
+                count++;                       // count files
+                strcat(subjects, ent->d_name); // save them all into subjects
+                strcat(subjects, "\n");
+            }
+        }
+        sprintf(c, "%d", count); // convert count int into char array
+        strcat(c, "\n");
+        strcat(c, subjects); // concenate count and subjects
+        bzero(subjects, BUF);
+        closedir(dir);
+    } else {
+        perror("Error when opening directory");
+        error = true;
+    }
+    printf("Subjects:\n%s", c);
+
+    if (count == 0 || error) {
+        bzero(c, BUF);
+        strcat(c, "0\n"); // when no user or files -> 0
+    }
+
+    if (send(*current_socket, c, BUF, 0) == -1) // send message
+    {
+        perror("list error");
+    }
+    bzero(filename, BUF);
+    bzero(c, BUF);
+}
+
+void Server::handleSend(char buffer[1024], const int *current_socket, long size, const char *directory, char *filename,
+                        FILE *fptr, bool error) {
+    char receiver[BUF]; // username of receiver
+    bzero(buffer, BUF);
+
+    for (int i = 0; i < 4; i++) // receive all parts of send (sender, receiver, subject, message)
+    {
+        size = recv(*current_socket, buffer, BUF, 0);
+        if (size <= 0) {
+            perror("recv error");
+            break;
+        }
+        printf("Information received: %s", buffer);
+
+        if (i == 0) {
+            buffer[strlen(buffer) - 1] = '\0';
+            strcpy(filename, directory); // get path of directory for user
+            strcat(filename, "/");
+            strcat(filename, buffer);
+            // printf("filename: %s\n", filename);
+            if (mkdir(filename, 0777) == -1) // make directory
+            {
+                if (errno == EEXIST) // ignore error if directory already exists
+                {
+                } else {
+                    error = true; // all other errors set bool on true
+                }
+            }
+        } else if (i == 1 && error == false) {
+            strcpy(receiver, buffer); // save username of receiver
+            // printf("receiver: %s\n", receiver);
+        } else if (i == 2 && error == false) {
+            buffer[strlen(buffer) - 1] = '\0';
+            strcat(filename, "/");
+            strcat(filename, buffer); // get path for file (named after subject)
+            // printf("filename: %s\n", filename);
+            fptr = fopen(filename, "w"); // create file
+            if (fptr == NULL) {
+                printf("Unable to create file.\n");
+                error = true;
+            } else {
+                fputs(receiver, fptr); // print receiver into file
+            }
+        } else if (i == 3 && error == false) {
+            fputs(buffer, fptr); // print message into file
+            fclose(fptr);
+        }
+    }
+
+    bzero(filename, BUF);
+    // printf("filename1: %s\n", filename);
+    bzero(receiver, BUF);
+    bzero(buffer, BUF);
+    if (!error) {
+        printf("\n\nEverything went well!\n");
+        buffer = (char *) "OK\n";
+    } else {
+        printf("\n\nThere was an error!\n");
+        buffer = (char *) "ERR\n";
+    }
+    if (send(*current_socket, buffer, BUF, 0) == -1) // send message to client
+    {
+        perror("send error");
+    }
 }
--- twmailer/server/server.h
+++ twmailer/server/server.h
@@ -35,14 +35,26 @@ public:
     void start_listening();                             // wait for connections
     void abort();                                        // end connection
     void handle_client_communication(int *parameterSocket);      // communicate with client
-    void mailer(char buffer[BUF], int *parameterSocket); // handles the mailer functions (SEND, ...)
+    void handle_command(char buffer[BUF], int *parameterSocket); // handles the handle_command functions (SEND, ...)
     int port{};
-    char *spoolDir{}; // spool directory of mailer
+    char *spoolDir{}; // spool directory of handle_command
     bool check_dir() const;
 
     bool create_server_socket() const;
 
     bool create_server_socket();
+
+    static void
+    handleSend(char *buffer, const int *current_socket, long size, const char *directory, char *filename, FILE *fptr,
+               bool error) ;
+
+    static void handleList(char *buffer, const int *current_socket, long size, const char *directory, char *filename,
+                    bool error) ;
+
+    static void handleRead(char *buffer, const int *current_socket, const char *directory, char *filename, FILE *fptr,
+                    bool error) ;
+
+    static void handleDel(char *buffer, const int *current_socket, const char *directory, char *filename, bool error) ;
 };
 
 #endif
